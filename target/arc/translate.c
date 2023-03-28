c9d143cde5729c01046acab08999f02743532236
BrunoASMauricio <brunoasmauricio@gmail.com>
2023-03-28 14:22:09 +0100

    FPU: Implemented FPU instructions for arcv3 (hs6x)
    Co-authored-by: Cupertino Miranda <cmiranda@synopsys.com>
    Signed-off-by: Bruno Mauricio <brunoasmauricio@gmail.com>

--- target/arc/translate.c
+++ target/arc/translate.c
@@ -55,6 +55,7 @@ TCGv    cpu_lps;
 TCGv    cpu_lpe;
 
 TCGv    cpu_r[64];
+TCGv    cpu_fpr[32];
 
 TCGv    cpu_intvec;
 
@@ -195,6 +196,16 @@ void arc_translate_init(void)
                                       ARC_REG_OFFS(r[i]),
                                       strdup(name));
     }
+
+    for (i = 0; i < 32; i++) {
+        char name[16];
+
+        sprintf(name, "fpr[%d]", i);
+        cpu_fpr[i] = tcg_global_mem_new(cpu_env,
+                                        ARC_REG_OFFS(fpr[i]),
+                                        strdup(name));
+    }
+
 
 #undef ARC_REG_OFFS
 #undef NEW_ARC_REG
@@ -504,6 +515,10 @@ static TCGv arc_decode_operand(const struct arc_opcode *opcode,
     } else {
         operand_t operand = ctx->insn.operands[nop];
 
+        if (operand.type & ARC_OPERAND_FP) {
+            return cpu_fpr[operand.value];
+        }
+
         if (operand.type & ARC_OPERAND_IR) {
             ret = cpu_r[operand.value];
             if (operand.value == 63) {
@@ -521,9 +536,9 @@ static TCGv arc_decode_operand(const struct arc_opcode *opcode,
                 ret = tcg_const_local_tl(limm);
             }
         }
-      }
+    }
 
-  return ret;
+    return ret;
 }
 
 /* See translate.h. */
